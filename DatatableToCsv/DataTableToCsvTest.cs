ea421bfd7e3b495b19374c7a72747276d9f8446c
Jon Graf <jongraf1@gmail.com>
2016-04-24 18:33:24 -0500

    Added additional test coverage for other value types

--- DatatableToCsv/DataTableToCsvTest.cs
+++ DatatableToCsv/DataTableToCsvTest.cs
@@ -51,6 +51,31 @@ namespace DatatableToCsv
             Assert.AreEqual("Column1,Column2,\ntestRow1,testRow2,", actual);
         }
 
+        [Test]
+        public void AddMutipleRowsToCsv()
+        {
+            _dataTable.Rows.Add("testRow1", "testRow2");
+            _dataTable.Rows.Add("testRow3", "testRow4");
 
+            var actual = _dataToCsv.ToCsv(_dataTable);
+
+            Assert.AreEqual("Column1,Column2,\ntestRow1,testRow2,\ntestRow3,testRow4,", actual);
+        }
+
+        [Test]
+        public void AddMultipleValueTyoes()
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable.Columns.Add("DateTime", typeof (DateTime));
+            dataTable.Columns.Add("Int", typeof (int));
+            dataTable.Columns.Add("Bool", typeof (bool));
+
+            dataTable.Rows.Add(DateTime.Parse("02/15/2016"), 123, true);
+
+            var actual = _dataToCsv.ToCsv(dataTable);
+
+            Assert.AreEqual("DateTime,Int,Bool,\n2/15/2016 12:00:00 AM,123,True,", actual);
+        }
     }
 }
