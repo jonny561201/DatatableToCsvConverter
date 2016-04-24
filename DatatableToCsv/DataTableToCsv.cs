6700d0b9d575a47c124a85ad924156f85db6b650
Jon Graf <jongraf1@gmail.com>
2016-04-24 16:19:13 -0500

    Initial Commit Test Driven attempt to convert datatable to csv

---
+++ DatatableToCsv/DataTableToCsv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatableToCsv
+{
+    public class DataTableToCsv
+    {
+        public string ToCsv(DataTable table)
+        {
+            var test = "";
+            foreach (var column in table.Columns)
+            {
+                if (!string.IsNullOrEmpty(column.ToString()))
+                    test += string.Concat(column.ToString(), ',');
+            }
+            foreach (DataRow row in table.Rows)
+            {
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    if(!string.IsNullOrEmpty(row.ToString()))
+                        test += string.Concat(row[i],'\n');
+                }
+            }
+            return test;
+        }
+    }
+}
---
+++ DatatableToCsv/DataTableToCsvTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DatatableToCsv
+{
+    [TestFixture]
+    public class DataTableToCsvTest
+    {
+        private DataTableToCsv _dataToCsv;
+        private DataTable _dataTable;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dataToCsv = new DataTableToCsv();
+            _dataTable = new DataTable();
+            _dataTable.Columns.Add("Column1", typeof(String));
+            _dataTable.Columns.Add("Column2", typeof(String));
+        }
+
+        [Test]
+        public void ConvertSingleRowOfDataTableToCsv()
+        {
+            var actual = _dataToCsv.ToCsv(_dataTable);
+
+            Assert.AreEqual("Column1,Column2,", actual);
+        }
+
+        [Test]
+        public void DoesNotCreateCsvForEmptyTable()
+        {
+            DataTable dataTable = new DataTable();
+
+            var actual = _dataToCsv.ToCsv(dataTable);
+
+            Assert.AreEqual("", actual);
+        }
+
+        [Test]
+        public void AddsReturnCarrageAtEndOfColumn()
+        {
+            _dataTable.Rows.Add("testRow1", "testRow2");
+
+            var actual = _dataToCsv.ToCsv(_dataTable);
+
+            Assert.AreEqual("Column1,Column2,\ntestRow1,testRow2,", actual);
+        }
+
+    }
+}
